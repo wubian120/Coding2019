18c3fad7f029c2a6fc80d02b1fea79ddc07540a7
Richard.Wu <120793495@qq.com>
2019-06-27 21:38:22 +0800

    prac 141

---
+++ src/main/java/cn/bw/leetcode/A141one.java
@@ -0,0 +1,32 @@
+package cn.bw.leetcode;
+
+/**
+ * 环形链表
+ */
+public class A141one {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head){
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
