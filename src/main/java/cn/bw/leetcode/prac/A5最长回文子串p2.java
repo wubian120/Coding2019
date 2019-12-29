65d0ed9c04a5862129e4be8acaec736ba4738397
Richard.Wu <120793495@qq.com>
2019-12-29 12:55:19 +0800

    reading  32最长有效括号， fuck  it's a hard one

---
+++ src/main/java/cn/bw/leetcode/prac/A5最长回文子串p2.java
@@ -0,0 +1,4 @@
+package cn.bw.leetcode.prac;
+
+public class A5最长回文子串p2 {
+}
---
+++ src/main/java/cn/bw/leetcode/sample/A32最长有效括号s.java
@@ -0,0 +1,40 @@
+package cn.bw.leetcode.sample;
+
+public class A32最长有效括号s {
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0
+                        && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1]
+                            + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0)
+                            + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
+
+
+    public static void main(String[] args){
+
+        String s = "())((())";
+        String s1 = "()()(())";
+
+        A32最长有效括号s a = new A32最长有效括号s();
+        int result = a.longestValidParentheses(s);
+
+        System.out.println(result);
+
+    }
+
+
+
+}
