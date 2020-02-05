2f04a5858e732edbdf3e35d5843ae07cc34cf2cb
Richard.Wu <120793495@qq.com>
2020-02-06 00:42:01 +0800

    prac 96, 978

--- src/main/java/cn/bw/leetcode/sample/A96不同的二叉搜索树s.java
+++ src/main/java/cn/bw/leetcode/sample/A96不同的二叉搜索树s.java
@@ -11,22 +11,17 @@ public class A96不同的二叉搜索树s {
 
 
     public int numTrees(int n) {
-
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int[] nums = new int[n + 1];
-        nums[0] = 1;
-        nums[1] = 1;
-
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < i; j++) {
-                nums[i] = nums[i] + nums[j] * nums[i - 1 - j];
+                dp[i] = dp[i] + dp[j] * dp[i - j - 1];
             }
         }
-        return nums[n];
+        return dp[n];
     }
-
 
 
 }
