433211cc2e77e1005840893f68e27068b72bcfad
Richard.Wu <120793495@qq.com>
2020-02-09 07:47:09 +0800

    prac 87

---
+++ src/main/java/cn/bw/leetcode/prac/p202002/A87扰乱字符串p5.java
@@ -0,0 +1,48 @@
+package cn.bw.leetcode.prac.p202002;
+
+/**
+ * @ClassName : A87扰乱字符串p5
+ * @Description :
+ * @Author : Richard.Wu
+ * @Date: 2020-02-08 23:50
+ */
+
+public class A87扰乱字符串p5 {
+
+    public boolean isScramble(String s1, String s2) {
+
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 != len2) {
+            return false;
+        }
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int[] letters = new int[26];
+        for (int i = 0; i < len1; i++) {
+            letters[s1.charAt(i) - 'a']++;
+            letters[s2.charAt(i) - 'a']--;
+        }
+
+        for (int i : letters) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        for (int i = 1; i < len1; i++) {
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+                    isScramble(s1.substring(i), s2.substring(i))) {
+                return true;
+            }
+
+            if (isScramble(s1.substring(0, i), s2.substring(len2 - i)) &&
+                    isScramble(s1.substring(i), s2.substring(0, len2 - i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
