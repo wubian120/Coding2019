0b84439dd91f2b4d1a07bd6b29046413a96ce5e7
Richard.Wu <120793495@qq.com>
2020-02-09 11:19:39 +0800

    p 91

---
+++ src/main/java/cn/bw/leetcode/prac/p202002/A91解码方法p0.java
@@ -0,0 +1,36 @@
+package cn.bw.leetcode.prac.p202002;
+
+/**
+ * @ClassName : A91解码方法p0
+ * @Description :
+ * @Author : Richard.Wu
+ * @Date: 2020-02-09 09:15
+ */
+
+public class A91解码方法p0 {
+
+    public int numDecodings(String s) {
+        if(s==null||s.length()==0){
+            return 0;
+        }
+        int len = s.length();
+        int[] dp = new int[len + 1];
+
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for(int i=2;i<=len;i++){
+            if(s.charAt(i-1)!='0'){
+                dp[i]=dp[i-1];
+            }
+
+            int twoDigits=(s.charAt(i-2)-'0')*10+(s.charAt(i-1)-'0');
+            if(twoDigits>=10 && twoDigits<=26){
+                dp[i]=dp[i]+dp[i-2];
+            }
+        }
+
+        return dp[len];
+
+    }
+}
