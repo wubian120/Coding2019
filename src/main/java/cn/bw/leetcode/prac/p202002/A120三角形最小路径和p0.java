cfc08085195a59d67ccd37f15c9092ae1c223193
Richard.Wu <120793495@qq.com>
2020-02-09 11:39:29 +0800

    p120

---
+++ src/main/java/cn/bw/leetcode/prac/p202002/A120三角形最小路径和p0.java
@@ -0,0 +1,30 @@
+package cn.bw.leetcode.prac.p202002;
+
+import java.util.List;
+
+/**
+ * @ClassName : A120三角形最小路径和p0
+ * @Description :
+ * @Author : Richard.Wu
+ * @Date: 2020-02-09 11:21
+ */
+
+public class A120三角形最小路径和p0 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int rows=triangle.size();
+
+        int path=0;
+        int[] dp=new int[rows+1];
+
+        for(int r=rows-1;r>=0;r--){
+            int cols=triangle.get(r).size();
+            for(int c=0;c<cols;c++){
+                dp[c]=triangle.get(r).get(c)+Math.min(dp[c],dp[c+1]);
+            }
+        }
+
+        return dp[0];
+
+    }
+}
--- src/main/java/cn/bw/leetcode/sample/A120三角形最小路径和s.java
+++ src/main/java/cn/bw/leetcode/sample/A120三角形最小路径和s.java
@@ -1,5 +1,7 @@
 package cn.bw.leetcode.sample;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class A120三角形最小路径和s {
@@ -7,15 +9,47 @@ public class A120三角形最小路径和s {
 
         int rows = triangle.size();
 
+        //注意：  rows+1;
+        //因为根据 三角形， 所以 每一列 应该 是 行数索引+1
         int[] dp = new int[rows+1];
 
-        for(int r=rows-1;r>=0;r--){
-            int cols= triangle.get(r).size();
-            for(int c = 0;c<cols;c++){
-                dp[c] = triangle.get(r).get(c) + Math.min(dp[c],dp[c+1]);
+        for (int r = rows - 1; r >= 0; r--) {
+            int cols = triangle.get(r).size();
+            for (int c = 0; c < cols; c++) {
+                //注意 dp[c+1], 如果 dp初始化 不是row+1，此处会越界
+                dp[c] = triangle.get(r).get(c) + Math.min(dp[c], dp[c + 1]);
             }
         }
 
         return dp[0];
+    }
+
+    public static void main(String... args) {
+
+        Integer[][] nums = {
+                {2},
+                {3, 4},
+                {6, 5, 7},
+                {4, 1, 8, 3}
+        };
+        List<Integer> t3 = Arrays.asList(nums[3]);
+        List<Integer> t2 = Arrays.asList(nums[2]);
+        List<Integer> t1 = Arrays.asList(nums[1]);
+        List<Integer> t0 = Arrays.asList(nums[0]);
+
+        List<List<Integer>> tri = new LinkedList<>();
+
+        tri.add(t0);
+        tri.add(t1);
+        tri.add(t2);
+        tri.add(t3);
+
+        A120三角形最小路径和s a = new A120三角形最小路径和s();
+
+        int result = a.minimumTotal(tri);
+
+        System.out.println(result);
+
+
     }
 }
