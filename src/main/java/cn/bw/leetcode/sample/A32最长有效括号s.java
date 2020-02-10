3dfd39693716a22af8f36def4fe548fdc8a0a4f4
Richard.Wu <120793495@qq.com>
2020-02-10 22:00:18 +0800

    p32  不熟练啊  需要继续练习

--- src/main/java/cn/bw/leetcode/sample/A32最长有效括号s.java
+++ src/main/java/cn/bw/leetcode/sample/A32最长有效括号s.java
@@ -5,10 +5,16 @@ public class A32最长有效括号s {
     public int longestValidParentheses(String s) {
         int maxans = 0;
         int dp[] = new int[s.length()];
+
+        //从1 开始遍历，
         for (int i = 1; i < s.length(); i++) {
+            //如果是有效的括号， 最左必然是 (
             if (s.charAt(i) == ')') {
+
                 if (s.charAt(i - 1) == '(') {
+
                     dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+
                 } else if (i - dp[i - 1] > 0
                         && s.charAt(i - dp[i - 1] - 1) == '(') {
                     dp[i] = dp[i - 1]
