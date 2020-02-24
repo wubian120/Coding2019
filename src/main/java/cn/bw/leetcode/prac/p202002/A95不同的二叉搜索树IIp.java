18279f3dd134f9ad2a1db20a5705f551e06c4777
Richard.Wu <120793495@qq.com>
2020-02-25 00:08:36 +0800

    prac 96, 95

--- src/main/java/cn/bw/leetcode/prac/p202002/A95不同的二叉搜索树IIp.java
+++ src/main/java/cn/bw/leetcode/prac/p202002/A95不同的二叉搜索树IIp.java
@@ -1,16 +1,17 @@
 package cn.bw.leetcode.prac.p202002;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * @ClassName : A95r1
+ * @ClassName : A不同的二叉搜索树IIp0
  * @Description :
  * @Author : Richard.Wu
- * @Date: 2020-02-07 11:52
+ * @Date: 2020-02-11 11:55
  */
 
-public class A95r1 {
+public class A95不同的二叉搜索树IIp {
 
     public class TreeNode {
         int val;
@@ -23,27 +24,27 @@ public class A95r1 {
     }
 
     public List<TreeNode> generateTrees(int n) {
-        if(n==0){
-            return new LinkedList<>();
-        }
+        if(n==0)return Collections.emptyList();
+
         return generate(1,n);
 
     }
 
-    private List<TreeNode> generate(int start, int end) {
-        List<TreeNode> tree = new LinkedList<>();
-        if (start > end) {
+    List<TreeNode> generate(int start, int end){
+        List<TreeNode> tree=new LinkedList<>();
+        if(start>end){
             tree.add(null);
             return tree;
         }
 
-        for(int i=start;i<=end;i++){
+        for(int i=start; i<=end; i++){
 
-            List<TreeNode> left=generate(start,i-1);
-            List<TreeNode> right=generate(i+1,end);
+            List<TreeNode> left=generate(start, i-1);
+            List<TreeNode> right = generate(i+1,end);
 
             for(TreeNode l:left){
                 for(TreeNode r:right){
+
                     TreeNode root=new TreeNode(i);
                     root.left=l;
                     root.right=r;
@@ -53,6 +54,18 @@ public class A95r1 {
 
         }
         return tree;
+
+
+    }
+
+
+
+    public static void main(String[] args){
+        int n= 10;
+
+        A95不同的二叉搜索树IIp a = new A95不同的二叉搜索树IIp();
+        List<TreeNode> tree= a.generateTrees(n);
+
     }
 
 
