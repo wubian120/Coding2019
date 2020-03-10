24cca6ed9fa567ddb1450c8c2a2cf9f2a72c210e
Richard.Wu <120793495@qq.com>
2020-03-10 09:24:04 +0800

    prac

--- src/main/java/cn/bw/leetcode/prac/p202002/A91解码方法p0.java
+++ src/main/java/cn/bw/leetcode/prac/p202002/A91解码方法p0.java
@@ -12,14 +12,11 @@ public class A91解码方法p0 {
 
     public int numDecodings(String s) {
 
-        if (s == null || s.length() == 0) return 0;
-
         int len = s.length();
-
         int[] dp = new int[len + 1];
 
         dp[0] = 1;
-        dp[1] = s.charAt(0) - '0' == 0 ? 0 : 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
 
         for (int i = 2; i <= len; i++) {
             if (s.charAt(i - 1) != '0') {
@@ -28,7 +25,7 @@ public class A91解码方法p0 {
 
             int twoDigits = (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0';
             if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i]+=dp[i-2];
+                dp[i] += dp[i - 2];
             }
         }
 
